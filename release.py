a817d2b0234d2cc650c43af3eeb1a5882e9d8a2f
Eyad Ahmed <dev.esnosy@gmail.com>
2024-10-01 03:32:34 +0300

    Update CI/CD related code and configuration

--- release.py
+++ release.py
@@ -4,18 +4,19 @@ import argparse
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Build and package a release build.")
+    parser = argparse.ArgumentParser(description="Build and package a release build")
     parser.add_argument("--build-dir", required=True)
-    parser.add_argument("--cpack-out-dir", help="Directory to store resultant CPack packages.", required=True)
+    parser.add_argument("--cpack-out-dir", help="Directory to store resultant CPack packages", required=True)
+    parser.add_argument("--generator", help="A valid CPack generator", required=True)
+    parser.add_argument("--pack-source", help="Create a source package instead of building the project",
+                        action="store_true")
     parser.add_argument("--cert-thumb",
-                        help="MD1 thumbprint of the Certum Cloud Certificate to use for signing binaries on Windows.")
+                        help="MD1 thumbprint of the Certum Cloud Certificate to use for signing binaries on Windows",
+                        required=False)
     args = parser.parse_args()
     cert_thumb = args.cert_thumb
 
-    cpack_out_dir = Path(args.cpack_out_dir)
-    if not (cpack_out_dir.exists() and cpack_out_dir.is_dir()):
-        print("--cpack-out-dir must be an existing directory.")
-        return 1
+    cpack_out_dir = args.cpack_out_dir
 
     parent_dir = Path(__file__).parent
     source_dir = parent_dir
@@ -34,9 +35,12 @@ def main():
     if cert_thumb:
         config_cmd += [f"-DQRCAT_CERTUM_CLOUD_CERT_MD1_THUMBPRINT={cert_thumb}"]
     sp.run(config_cmd, check=True)
-    build_cmd = ["cmake", "--build", build_dir_posix]
-    sp.run(build_cmd, check=True)
-    cpack_cmd = ["cpack", "-G", "ZIP", "-B", cpack_out_dir.as_posix()]
+    if not args.pack_source:
+        build_cmd = ["cmake", "--build", build_dir_posix]
+        sp.run(build_cmd, check=True)
+    cpack_cmd = ["cpack", "-G", args.generator, "-B", cpack_out_dir]
+    if args.pack_source:
+        cpack_cmd += ["--config", "CPackSourceConfig.cmake"]
     sp.run(cpack_cmd, check=True, cwd=build_dir)
     return 0
 
